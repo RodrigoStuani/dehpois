321c5672a2441d96037bf967be2ee1f1662af619
Rodrigo <stuani.developer@gmail.com>
2020-09-25 02:29:54 -0300

    Adicionado hooks de state para o botão switch.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-//import { StatusBar, View} from 'react-native';
 
 import Routes from './routes';
-//import Main from './pages/Main';
 
 export default function App() {
   return (
